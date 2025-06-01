5ee5dcad1c64455c0e02ed3d4a0145080427406b
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-06-01 18:09:31 +0200

    update

---
+++ C/printf/notes.md
@@ -0,0 +1,9 @@
+# In depth printf
+
+## Printf modifiers
+
+File [sample-1.c](./sample-1.c) is a compact useful reference for printf modifiers.
+
+## Printf variants
+
+File [sample-2.c](./sample-2.c) is a compact useful reference for printf variants.
--- C/printf/sample-1.c
+++ C/printf/sample-1.c
@@ -3,17 +3,34 @@
 int main() {
     int a = 1;
 
-    printf("%d\n", -32);                // Data types
-    printf("%u\n", 32);
-    printf("%f\n", 42.42);
-    printf("%p\n", &a);
-    printf("%c\n", 'a');
-    printf("%s\n", "hello");
-    printf("%%\n");
-    printf("%g\n", 139862385.218487);
+    printf("%d\n", -32);                // Int
+    printf("%u\n", 32);                 // Unsigned int
+    printf("%f\n", 42.42);              // Float
+    printf("%lf\n", 42.42);             // Double
+    printf("%p\n", &a);                 // Pointer
+    printf("%c\n", 'a');                // Char
+    printf("%s\n", "hello");            // String
+    printf("%%\n");                     // %
+    printf("%g\n", 139862385.218487);   // Scientific 
+    long int l = 8654886149842631684;           // Long 
+    printf("%ld\n", l);
+    long long int ll = 8654886149842631684;     // Long long
+    printf("%lld\n", ll);               
+    short s = 123;                              // Short
+    printf("%hd\n", s);
+    char c = 12;                                // Numeric char
+    printf("%hhd\n", c);                // It prints chars like decimals
+    size_t n = 5746;                            // Platform indipendent unsigned int
+    printf("%zu\n", n);     
+
+    printf("%.3f\n", 42.42654651);      // Precision
+    printf("%.*f\n", 5, 42.42654651);   // Precision by parameter
 
     printf("##%5d\n", 634);             // Left padding
     printf("%-5d##\n", -32);            // Right padding
+    printf("%*d\n", 5, 42);             // Padding by parameter
+    printf("#%*d\n", 8, 42);
+    printf("%*.*f\n", 3, 5, 42.42654651);
 
     printf("%05d\n", 32);               // 0 padding
     printf("%05d\n", -32);
