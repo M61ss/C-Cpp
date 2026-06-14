204e3e6b745a255d00fbc651180076b0c2165651
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-06-14 20:04:53 +0200

    create a basis for main

--- C++/elias/src/main.cc
+++ C++/elias/src/main.cc
@@ -1,6 +1,37 @@
+#include <fstream>
+
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc != 4)
+    {
+        return 1;
+    }
+
+    std::string command(argv[1]);
+    std::ifstream is(argv[2], std::ios::binary);
+    if (!is)
+    {
+        return 1;
+    }
+    std::ofstream os(argv[3], std::ios::binary);
+    if (!os)
+    {
+        return 1;
+    }
+
+    if (command == "-c" || command == "--compress")
+    {
+
+    }
+    else if (command == "-d" || "--decompress")
+    {
+
+    }
+    else if (command == "-h" || command == "--help")
+    {
+
+    }
+    else
     {
         return 1;
     }
