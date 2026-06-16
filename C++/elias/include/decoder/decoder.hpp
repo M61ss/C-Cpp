4f90651d59fe5f890c712fd3f4a2cf4b96370717
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-06-16 23:27:56 +0200

    basic decoder

--- C++/elias/include/decoder/decoder.hpp
+++ C++/elias/include/decoder/decoder.hpp
@@ -1,6 +1,19 @@
 #ifndef ELIAS_DECODER_DECODER_H_
 #define ELIAS_DECODER_DECODER_H_
 
+#include <fstream>
 
+#include "bitreader.hpp"
+
+namespace elias_decoder {
+	class Decoder {
+	public:
+		Decoder(std::istream& is, bit_reader::Bitreader& br);
+
+	private:
+		std::istream& is_;
+		bit_reader::Bitreader& br_;
+	};
+}
 
 #endif // ELIAS_DECODER_DECODER_H_
--- C++/elias/src/decoder/decoder.cc
+++ C++/elias/src/decoder/decoder.cc
@@ -1,1 +1,3 @@
 #include "decoder.hpp"
+
+elias_decoder::Decoder::Decoder(std::istream& is, bit_reader::Bitreader& br) : is_(is), br_(br) {}
