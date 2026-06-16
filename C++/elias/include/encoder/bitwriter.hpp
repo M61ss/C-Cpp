675f21e6cc206ba16767e27d56b1234e1dace94d
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-06-16 22:49:30 +0200

    complete bitwriter

--- C++/elias/include/encoder/bitwriter.hpp
+++ C++/elias/include/encoder/bitwriter.hpp
@@ -1,6 +1,21 @@
 #ifndef ELIAS_ENCODER_BITWRITER_H_
 #define ELIAS_ENCODER_BITWRITER_H_
 
+#include <fstream>
 
+namespace bit_writer {
+	class BigEndian {
+	public:
+		BigEndian(std::ostream& os);
+		void writeBit(const uint8_t& bit);
+		void writeSequence(const size_t& val, const size_t& length);
+		void flush(const uint8_t& bit = 0);
+
+	private:
+		std::ostream& os_;
+		uint8_t buffer_;
+		size_t n_;
+	};
+}
 
 #endif // ELIAS_ENCODER_BITWRITER_H_
--- C++/elias/src/encoder/bitwriter.cc
+++ C++/elias/src/encoder/bitwriter.cc
@@ -1,1 +1,24 @@
 #include "bitwriter.hpp"
+
+bit_writer::BigEndian::BigEndian(std::ostream& os) : os_(os), buffer_(0), n_(0) {}
+
+void bit_writer::BigEndian::writeBit(const uint8_t& bit) {
+	buffer_ = (buffer_ << 1) | bit;
+	++n_;
+	if (n_ == 8) {
+		os_.write(reinterpret_cast<char*>(&buffer_), 1);
+		n_ = 0;
+	}
+}
+
+void bit_writer::BigEndian::writeSequence(const size_t& val, const size_t& length) {
+	for (int i = static_cast<int>(length) - 1; i >= 0; i++) {
+		writeBit((val >> i) & 1);
+	}
+}
+
+void bit_writer::BigEndian::flush(const uint8_t& bit = 0) {
+	while (n_ > 0) {
+		writeBit(bit);
+	}
+}
