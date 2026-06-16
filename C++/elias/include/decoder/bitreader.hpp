afb890d03c7583daf0f4158875f40c77c86107bd
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-06-16 19:18:28 +0200

    complete bitreader

--- C++/elias/include/decoder/bitreader.hpp
+++ C++/elias/include/decoder/bitreader.hpp
@@ -6,6 +6,20 @@
 namespace bit_reader {
     class BigEndian {
     public:
+        BigEndian(std::istream& is);
+        uint8_t readBit();
+        size_t readSequence(const size_t& length);
+        void flush();
+
+    private:
+        std::istream& is_;
+        uint8_t buffer_;
+        size_t n_;
+    };
+
+    class LittleEndian {
+    public:
+        LittleEndian(std::istream& is);
         uint8_t readBit();
         size_t readSequence(const size_t& length);
         void flush();
--- C++/elias/src/decoder/bitreader.cc
+++ C++/elias/src/decoder/bitreader.cc
@@ -1,5 +1,7 @@
 #include "bitreader.hpp"
 
+bit_reader::BigEndian::BigEndian(std::istream& is) : is_(is), buffer_(0), n_(0) {}
+
 uint8_t bit_reader::BigEndian::readBit() {
 	if (n_ == 0) {
 		is_.read(reinterpret_cast<char*>(&buffer_), 1);
@@ -18,6 +20,33 @@ size_t bit_reader::BigEndian::readSequence(const size_t& length) {
 }
 
 void bit_reader::BigEndian::flush() {
+	buffer_ = 0;
+	n_ = 0;
+}
+
+bit_reader::LittleEndian::LittleEndian(std::istream& is) : is_(is), buffer_(0), n_(0) {}
+
+uint8_t bit_reader::LittleEndian::readBit() {
+	if (n_ == 0) {
+		is_.read(reinterpret_cast<char*>(&buffer_), 1);
+		n_ = 8;
+	}
+	--n_;
+	uint8_t bit = buffer_ & 1;
+	buffer_ >>= 1;
+	return bit;
+}
+
+size_t bit_reader::LittleEndian::readSequence(const size_t& length) {
+	size_t val = 0;
+	for (size_t i = 0; i < length; i++) {
+		size_t bit = readBit();
+		val |= bit << i;
+	}
+	return val;
+}
+
+void bit_reader::LittleEndian::flush() {
 	buffer_ = 0;
 	n_ = 0;
 }
